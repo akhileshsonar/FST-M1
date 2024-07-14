89a869c56d9e86855e80d4e1783c586c9cd2694a
akhileshsonar <Akhilesh.Sonar@ibm.com>
2024-07-14 23:38:11 +0530

    Update Activity_20.java

--- Selenium/Activities/Activity_20.java
+++ Selenium/Activities/Activity_20.java
@@ -1,54 +1,30 @@
 package test_basic_practice;
 
 import java.time.Duration;
-import java.util.List;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Activity_20 {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://v1.training-support.net/selenium/tab-opener");
-		System.out.println("Title is: " + driver.getTitle());
-		
-		//wait
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		
-        System.out.println("Original tab :" + driver.getWindowHandle());
-        driver.findElement(By.id("launcher")).click();
-        wait.until(ExpectedConditions.numberOfWindowsToBe(2));
-        System.out.println("new open windows :" + driver.getWindowHandles());
-        for(String handle : driver.getWindowHandles()) {
-            driver.switchTo().window(handle);
-        }
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("actionButton")));
-        System.out.println("Original tab: " + driver.getWindowHandle());
-        System.out.println("Page title: " + driver.getTitle());
-        String pageHeading = driver.findElement(By.className("content")).getText();
-        System.out.println("Page Heading: " + pageHeading);
-        driver.findElement(By.id("actionButton")).click();
-        wait.until(ExpectedConditions.numberOfWindowsToBe(3));
-        for(String handle : driver.getWindowHandles()) {
-            driver.switchTo().window(handle);
-        }
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("content")));
-        System.out.println("Current tab: " + driver.getWindowHandle());
-        System.out.println("Page title: " + driver.getTitle());
-        pageHeading = driver.findElement(By.className("content")).getText();
-        System.out.println("Page Heading: " + pageHeading);
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://v1.training-support.net/selenium/javascript-alerts");
+
+        System.out.println("Home page title: " + driver.getTitle());
+
+        driver.findElement(By.id("prompt")).click();
+
+        Alert promtAlert = driver.switchTo().alert();
+        String alertText = promtAlert.getText();
+        System.out.println("Text in alert: " + alertText);
+
+        promtAlert.sendKeys("Test Alert");
+
+        promtAlert.accept();
         driver.quit();
-		
-		
-		
-		
-		
-	}
-
+    }
 }
