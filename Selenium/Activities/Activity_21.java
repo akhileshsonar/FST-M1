ca73a47c39e6af2cf03759036252db6dd3a427b6
akhileshsonar <Akhilesh.Sonar@ibm.com>
2024-07-14 23:38:44 +0530

    Update Activity_21.java

--- Selenium/Activities/Activity_21.java
+++ Selenium/Activities/Activity_21.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -16,36 +15,35 @@ public class Activity_21 {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://v1.training-support.net/selenium/popups");
+		driver.get("https://v1.training-support.net/selenium/tab-opener");
 		System.out.println("Title is: " + driver.getTitle());
 		
-		//wait
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		Actions action = new Actions(driver);
 		
-		
-		WebElement button = driver.findElement(By.className("orange"));
-		action.moveToElement(button).build().perform();
-
-        String tooltipMessage = button.getAttribute("data-tooltip");
-        System.out.println(tooltipMessage);
-
-        button.click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("username")));
-
-        WebElement user = driver.findElement(By.id("username"));
-        WebElement pass = driver.findElement(By.id("password"));
-       
-        user.sendKeys("admin");
-        pass.sendKeys("password");
-
-        driver.findElement(By.xpath("//button[text()='Log in']")).click();
-
-        String message = driver.findElement(By.id("action-confirmation")).getText();
-        System.out.println("confirmations message: " + message);
-
+        System.out.println("Original tab :" + driver.getWindowHandle());
+        driver.findElement(By.id("launcher")).click();
+        wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+        System.out.println("new open windows :" + driver.getWindowHandles());
+        for(String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+        }
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("actionButton")));
+        System.out.println("Original tab: " + driver.getWindowHandle());
+        System.out.println("Page title: " + driver.getTitle());
+        String pageHeading = driver.findElement(By.className("content")).getText();
+        System.out.println("Page Heading: " + pageHeading);
+        driver.findElement(By.id("actionButton")).click();
+        wait.until(ExpectedConditions.numberOfWindowsToBe(3));
+        for(String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+        }
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("content")));
+        System.out.println("Current tab: " + driver.getWindowHandle());
+        System.out.println("Page title: " + driver.getTitle());
+        pageHeading = driver.findElement(By.className("content")).getText();
+        System.out.println("Page Heading: " + pageHeading);
         driver.quit();
-	
+			
 	}
 
 }
